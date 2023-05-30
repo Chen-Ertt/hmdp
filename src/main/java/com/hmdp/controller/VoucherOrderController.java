98e557da2b7b66aaa670b14a3f1917920dadfb1b
chenertt <xe85ii@163.com>
2023-05-30 23:31:03 +0800

    synchronized悲观锁，实现一人一单 解决事务和锁之间存在的问题

--- src/main/java/com/hmdp/controller/VoucherOrderController.java
+++ src/main/java/com/hmdp/controller/VoucherOrderController.java
@@ -3,8 +3,6 @@ package com.hmdp.controller;
 
 import com.hmdp.dto.Result;
 import com.hmdp.service.IVoucherOrderService;
-import com.hmdp.service.IVoucherService;
-import com.hmdp.service.impl.VoucherServiceImpl;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -9,6 +9,7 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.hmdp.utils.RedisIDWorker;
 import com.hmdp.utils.UserHolder;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,6 +30,10 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource
     private ISeckillVoucherService seckillVoucherService;
 
+    // 注入自己，是@Transactional能够生效
+    @Resource
+    private VoucherOrderServiceImpl voucherOrderService;
+
     // ID生成器
     @Resource
     private RedisIDWorker redisIDWorker;
@@ -37,12 +42,8 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
      * 基于乐观锁实现防止超卖问题
      * 使用库存stock作为version code
      * 将上锁转移给mysql（行锁）
-     *
-     * @param voucherId
-     * @return
      */
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
         // 1. 查询优惠券
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
@@ -62,6 +63,39 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         Integer stock = voucher.getStock();
         if(stock < 1) {
             return Result.fail("库存不足~");
+        }
+
+        // 对用户加锁：
+        // 所有用户用同一把锁，效率过低
+        // 对方法加锁：
+        // 考虑到先释放锁，后提交事务，会导致数据未被commit前，其他线程获取到锁，因此需要对整个方法加锁
+        Long userID = UserHolder.getUser().getId();
+        synchronized(userID.toString().intern()) {
+            return voucherOrderService.createVoucherOrder(voucherId);
+        }
+    }
+
+    /**
+     * synchronized保证线程安全，锁的粒度是用户
+     * 1. 先判断一人一单
+     * 2. 扣除库存
+     * 3. 创建订单并返回订单id
+     */
+    @NotNull
+    // 存在一个问题，就是 @Transactional 在此处不会生效
+    // 因为该注解功能的实现依赖于AOP：
+    // 首先需要导入aspect依赖，并开启spring事务管理功能
+    // 调用此方法是通过this.method()调用的，而this并非proxy对象，因此aop不生效
+    // 也就是说必须通过代理对象访问自己，并调用方法才ok
+    // 可以通过在类中注入自己
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        // 一人一单
+        Long userID = UserHolder.getUser().getId();
+
+        Long count = query().eq("user_id", userID).eq("voucher_id", voucherId).count();
+        if(count > 0) {
+            return Result.fail("购买数量现限制");
         }
 
         // 4. 扣除库存
@@ -84,10 +118,10 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
         order.setVoucherId(voucherId);
 
-        Long userID = UserHolder.getUser().getId();
         order.setUserId(userID);
         save(order);
 
         return Result.ok(orderID);
+
     }
 }
